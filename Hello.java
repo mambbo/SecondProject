0bb199ada0cc4dae799573c7a13666f7a18a9d7f
mambbo <mambbo@naver.com>
2017-01-24 14:44:07 +0900

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -7,8 +7,6 @@ public class Hello{
 		System.out.println("Hello Git!");
 		System.out.println("what is Git?");
 		System.out.println("That's DVCS");
-		System.out.println("Hello Git!");
-		System.out.println("what is Git?");
 		System.out.println("2002 Bitkeeper DVCS");
 
         }	
